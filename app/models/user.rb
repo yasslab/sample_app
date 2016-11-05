bf7cc2747fdd5c555152ddb3215819ee0529436b
Yohei Yasukawa <yasulab@gmail.com>
2016-11-05 00:36:17 +0000

    Finish ch08

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,11 @@ class User < ApplicationRecord
   validates :password, presence: true,
                          length: { minimum: 6},
                       allow_nil: true
+
+  # 与えられた文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
